746265cdeb55c1a8260a98ecd885edd6372e059c
Yanendrajha <yanendrajha37@gmail.com>
2024-07-27 16:01:23 +0530

    conn to db after using .env

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const app = express()
 const connectionMongoDB = require('./connection')
-
+require('dotenv').config();
 
 connectionMongoDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(3001,()=> {
+app.listen(5045,()=> {
     console.log('project Started');
 })
 
--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config();
-const url = "mongodb+srv://"+process.env.DB_USERNAME+":"+process.env.DB_PASSWORD+"@cluster0.ayi7fwq.mongodb.net/blogs"
 
+const url = "mongodb+srv://"+  process.env.DB_USERNAME + ":"+ process.env.DB_PASSWORD + "@cluster0.ayi7fwq.mongodb.net/blogs"
 
 async function connectionMongoDB() {
     return mongoose.connect(url).then(() => {
